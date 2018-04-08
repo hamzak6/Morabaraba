0312fbee6c148ea0d882e465571fcaf7264a635d
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-08 20:47:47 +0200

    Printing interface

---
+++ IPrinter.cs
@@ -0,0 +1,19 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// A Morabaraba printer
+    /// </summary>
+    public interface IPrinter
+    {
+        /// <summary>
+        /// Prints relevant game information to the user
+        /// </summary>
+        void Print();
+        
+        /// <summary>
+        /// Handles an error
+        /// </summary>
+        /// <param name="error">Description of the error</param>
+        void Error(string error);
+    }
+}
