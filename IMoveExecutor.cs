4a542b700afb8542bc34f02ec48da660ed3162e2
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:47:53 +0200

    Accept game as a method

--- IMoveExecutor.cs
+++ IMoveExecutor.cs
@@ -12,9 +12,10 @@ namespace Morabaraba
         /// </summary>
         /// <param name="move">The move to be executed</param>
         /// <param name="coordinates">The coordinates to be used for the execution</param>
+        /// <param name="game">The game state the move will be executed on</param>
         /// <returns>The next move</returns>
         /// <exception cref="ArgumentException">Thrown when coordinates are empty</exception>
         /// <exception cref="ArgumentNullException">Thrown when coordinates are null</exception>
-        Move Execute(Move move, Coordinate[] coordinates);
+        Move Execute(Move move, Coordinate[] coordinates, IGame game);
     }
 }
