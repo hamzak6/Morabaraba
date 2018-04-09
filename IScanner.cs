03b37dbb442da84a259d4f27f5dd009cf9293ceb
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:41:53 +0200

    Scanning interface

---
+++ IScanner.cs
@@ -0,0 +1,14 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// The scanning interface
+    /// </summary>
+    public interface IScanner
+    {
+        /// <summary>
+        /// Scan and parse coordinates input by the user
+        /// </summary>
+        /// <returns></returns>
+        Coordinate[] Scan();
+    }
+}
