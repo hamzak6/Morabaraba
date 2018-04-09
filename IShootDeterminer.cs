6780fadeeafc9aa88522ed10c6cb2ba7a6218593
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:52:06 +0200

    Shoot determiner interface

---
+++ IShootDeterminer.cs
@@ -0,0 +1,15 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// Shoot determiner interface
+    /// </summary>
+    public interface IShootDeterminer
+    {
+        /// <summary>
+        /// Can the player shoot?
+        /// </summary>
+        /// <param name="game">The game state</param>
+        /// <returns>Whether the player can shoot</returns>
+        bool CanShoot(IGame game);
+    }
+}
