23ba9ee658f326143366160d1bb25856159d8637
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 15:17:33 +0200

    Implement Game interface

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// An implementation of the Morabaraba Game interface
+    /// </summary>
+    public class Game : IGame
+    {
+        public IBoard Board { get; }
+
+        public IPlayer DarkPlayer { get; }
+
+        public IPlayer LightPlayer { get; }
+
+        public Move? LastPlayed { get; set; }
+
+        /// <summary>
+        /// Creates a new Morabaraba Game object
+        /// </summary>
+        /// <param name="board">The board to be used by the game</param>
+        /// <param name="darkPlayer">The dark player for the game</param>
+        /// <param name="lightPlayer">The light player for the game</param>
+        /// <param name="lastPlayed">The last move played</param>
+        public Game(IBoard board, IPlayer darkPlayer, IPlayer lightPlayer, Move? lastPlayed)
+        {
+            Board = board;
+            DarkPlayer = darkPlayer;
+            LightPlayer = lightPlayer;
+            LastPlayed = lastPlayed;
+        }
+    }
+}
