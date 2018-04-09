e58fd4bf5ba65fc1a78a5882b2f111f1ecaba374
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-10 00:03:05 +0200

    Add ArgumentException comment

--- IMoveExecutor.cs
+++ IMoveExecutor.cs
@@ -13,7 +13,7 @@ namespace Morabaraba
         /// <param name="move">The move to be executed</param>
         /// <param name="coordinates">The coordinates to be used for the execution</param>
         /// <returns>Potential error</returns>
-        /// <exception cref="ArgumentException">Thrown when move is game end move</exception>
+        /// <exception cref="ArgumentException">Thrown when the move passed is and end of game move</exception>
         string Execute(Move move, Coordinate[] coordinates);
     }
 }
