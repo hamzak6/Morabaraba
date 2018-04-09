e8038662e1fcfef6815d8c9c74be23ba17c0b14c
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 10:47:19 +0200

    Game state

---
+++ IGame.cs
@@ -0,0 +1,23 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// Morabaraba game state
+    /// </summary>
+    public interface IGame
+    {
+        /// <summary>
+        /// Morabaraba board state
+        /// </summary>
+        IBoard Board { get; }
+        
+        /// <summary>
+        /// Get a Morabaraba Dark Player
+        /// </summary>
+        IPlayer DarkPlayer { get; }
+        
+        /// <summary>
+        /// Get a Morabaraba Light Player
+        /// </summary>
+        IPlayer LightPlayer { get; }
+    }
+}
