8b97bc0d3ffc25767985189e6384c9d2eb3898ec
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 16:22:07 +0200

    Remove game parameter

--- IMoveExecutor.cs
+++ IMoveExecutor.cs
@@ -12,10 +12,9 @@ namespace Morabaraba
         /// </summary>
         /// <param name="move">The move to be executed</param>
         /// <param name="coordinates">The coordinates to be used for the execution</param>
-        /// <param name="game">The game state the move will be executed on</param>
         /// <returns>The next move</returns>
         /// <exception cref="ArgumentException">Thrown when coordinates are empty</exception>
         /// <exception cref="ArgumentNullException">Thrown when coordinates are null</exception>
-        Move Execute(Move move, Coordinate[] coordinates, IGame game);
+        Move Execute(Move move, Coordinate[] coordinates);
     }
 }
