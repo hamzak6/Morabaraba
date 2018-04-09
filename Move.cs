211ff1acb59a45cd771269a3edc8ed41d0fd6bb3
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:54:28 +0200

    Morabaraba moves and finishes

---
+++ Move.cs
@@ -0,0 +1,20 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// Morabaraba Moves (and Finishes)
+    /// </summary>
+    public enum Move
+    {
+        DarkPlace,
+        DarkMove,
+        DarkFly,
+        DarkShoot,
+        LightPlace,
+        LightMove,
+        LightFly,
+        LightShoot,
+        Draw,
+        DarkWins,
+        LightWins
+    }
+}
