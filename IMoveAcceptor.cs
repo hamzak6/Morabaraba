e673e0cf905f62ae15a885f5ee95a1e983af9acd
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 10:58:22 +0200

    Move acceptor interface

---
+++ IMoveAcceptor.cs
@@ -0,0 +1,13 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// Morabaraba move acceptor
+    /// </summary>
+    public interface IMoveAcceptor
+    {
+        /// <summary>
+        /// Accepts the correctly scanned input for a Morabaraba move
+        /// </summary>
+        void Accept();
+    }
+}
