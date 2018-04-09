95f9a0a4807a2a6f9f783aeb9092d262aed799c2
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:53:42 +0200

    Turn determining interface

---
+++ ITurnDeterminer.cs
@@ -0,0 +1,15 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// The turn determining interface
+    /// </summary>
+    public interface ITurnDeterminer
+    {
+        /// <summary>
+        /// Whose turn is it?
+        /// </summary>
+        /// <param name="game">The game state</param>
+        /// <returns>The colour of the player with the turn</returns>
+        Colour WhoseTurn(IGame game);
+    }
+}
