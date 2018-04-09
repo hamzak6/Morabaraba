f1792016d571c6eb7d0116cc6663b74223e6cff6
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:13:43 +0200

    Move executing interface

---
+++ IMoveExecutor.cs
@@ -0,0 +1,16 @@
+﻿namespace Morabaraba
+{
+    /// <summary>
+    /// Move executor interface
+    /// </summary>
+    public interface IMoveExecutor
+    {
+        /// <summary>
+        /// Executes the move
+        /// </summary>
+        /// <param name="move">The move to be executed</param>
+        /// <param name="coordinates">The coordinates to be used for the execution</param>
+        /// <returns>The next move</returns>
+        Move Execute(Move move, Coordinate[] coordinates);
+    }
+}
