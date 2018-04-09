175848c203ea90dfaabcbdfbac747be6ce7d45bc
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-10 00:32:47 +0200

    Implementation fo Morabaraba printer

---
+++ Printer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Morabaraba
+{
+    /// <summary>
+    /// Implementation of Morabaraba printer
+    /// </summary>
+    public class Printer : IPrinter
+    {
+        private readonly IGame _game;
+        
+        public void Print()
+        {
+            const string title = @"                                                  _                                                       _                                                 
+                 )))           xxx           ((_           ___           ,,,           ___           ((_           ___           ,,,           ___      
+                (o o)         (o o)         (o o)         (o o)         (o o)         (o o)         (o o)         (o o)         (o o)         (o o)     
+            ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+            ";
+            var board = _game.Board.ToString();
+            var error = 
+                $@"                                                            {Error}
+            ";
+            var request = $@"                                                    {Request}: ";
+            var display = title + board + error + request;
+            Console.Write(display);
+        }
+
+        public string Error { get; set; }
+        
+        public string Request { get; set; }
+        
+        public Printer (IGame game)
+        {
+            _game = game;
+        }
+    }
+}
