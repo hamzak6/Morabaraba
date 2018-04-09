f0ff28096b3727c82d05359bebc2c7477350009c
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-09 11:18:07 +0200

    Add exceptions

--- IMoveExecutor.cs
+++ IMoveExecutor.cs
@@ -1,4 +1,6 @@
-﻿namespace Morabaraba
+﻿using System;
+
+namespace Morabaraba
 {
     /// <summary>
     /// Move executor interface
@@ -11,6 +13,8 @@
         /// <param name="move">The move to be executed</param>
         /// <param name="coordinates">The coordinates to be used for the execution</param>
         /// <returns>The next move</returns>
+        /// <exception cref="ArgumentException">Thrown when coordinates are empty</exception>
+        /// <exception cref="ArgumentNullException">Thrown when coordinates are null</exception>
         Move Execute(Move move, Coordinate[] coordinates);
     }
 }
